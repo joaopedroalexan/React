35d436c8ef9140688054e46a479751b2c440a3cc
joaopedroalexan <joaopedroalexsilva@gmail.com>
2025-01-29 11:23:23 -0300

    first commit

---
+++ meuSiteBasico/src/App.jsx
@@ -0,0 +1,12 @@
+import Component1 from "./Component";
+function App() {
+  return (
+    <div>
+      <p>Joao</p>
+      <p>Alexandre</p>
+      <Component1/>
+    </div>
+  );
+}
+
+export default App;
